65f95fdc671a9a21e94075bd326f47a2de3ea7c1
Raphael Alves <rapha.tavares1010@gmail.com>
2021-04-30 15:53:39 -0300

    configurando postgresql

--- src/main/java/com/mxapi/DataConfiguration.java
+++ src/main/java/com/mxapi/DataConfiguration.java
@@ -1,7 +1,11 @@
 package com.mxapi;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -13,27 +17,45 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 	
-	@Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mxapi");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        return dataSource;
-    }
-	
+	  @Bean
+	    public BasicDataSource dataSource() throws URISyntaxException {
+	        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter(){
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL57Dialect");
-		adapter.setPrepareConnection(true);
-		return adapter;
-	}
+	        String username = dbUri.getUserInfo().split(":")[0];
+	        String password = dbUri.getUserInfo().split(":")[1];
+	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+	        BasicDataSource basicDataSource = new BasicDataSource();
+	        basicDataSource.setUrl(dbUrl);
+	        basicDataSource.setUsername(username);
+	        basicDataSource.setPassword(password);
+
+	        return basicDataSource;
+	    }
+
+	
+//	mysql
+//	@Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/mxapi");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
+//	
+//
+//	@Bean
+//	public JpaVendorAdapter jpaVendorAdapter(){
+//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//		adapter.setDatabase(Database.MYSQL);
+//		adapter.setShowSql(true);
+//		adapter.setGenerateDdl(true);
+//		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL57Dialect");
+//		adapter.setPrepareConnection(true);
+//		return adapter;
+//	}
 	
 	
 }
