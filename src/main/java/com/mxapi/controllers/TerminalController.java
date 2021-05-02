5f0f2304c93ce34a5b4d4bf927ba088c318e683b
Raphael Alves <rapha.tavares1010@gmail.com>
2021-05-01 21:15:56 -0300

    apontando o verbo delete para not found

--- src/main/java/com/mxapi/controllers/TerminalController.java
+++ src/main/java/com/mxapi/controllers/TerminalController.java
@@ -3,6 +3,7 @@ package com.mxapi.controllers;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,5 +79,11 @@ public class TerminalController {
 		return ResponseEntity.ok(tr.save(terminal));
 		
 	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deleteContato() {
+		return ResponseEntity.notFound().build();
+	}
+	
 
 }
