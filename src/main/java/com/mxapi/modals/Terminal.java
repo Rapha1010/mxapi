c6a9451eef6a05dc4fa8422885c62dc19a3ea4b1
Raphael Alves Tavares Leão <rapha.tavares1010@gmail.com>
2021-05-04 16:54:04 -0300

    add serializable

--- src/main/java/com/mxapi/modals/Terminal.java
+++ src/main/java/com/mxapi/modals/Terminal.java
@@ -1,5 +1,7 @@
 package com.mxapi.modals;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,7 +9,9 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-public class Terminal {
+public class Terminal implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
